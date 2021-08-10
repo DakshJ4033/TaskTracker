c1256ecaee1fb1911f7dfd9b4927a0d4ef985f20
DakshJ4033 <82127623+DakshJ4033@users.noreply.github.com>
2021-08-10 16:06:57 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTasks from './components/AddTask';
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+    // Fetch Data
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+      return data
+    }   
+
+    // Fetch Task
+    const fetchTask = async (id) => {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+
+      return data
+    }   
+
+    // Add Tasks
+    const addTask = async (task) => {
+      // const id = Math.floor(Math.random() * 10000) + 1
+      // const newTask = { id, ...task }
+      // setTasks([...tasks, newTask])
+
+      const res = await fetch('http://localhost:5000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+
+      const data = await res.json()
+
+      setTasks([...tasks, data])
+    }
+
+    // Delete Tasks
+    const delTasks = async (id) => {
+      await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'DELETE'
+      })
+
+      setTasks(tasks.filter((task) => task.id !== id)) 
+    }
+
+    // Toggle Reminder
+    const toggleReminder = async (id) => {
+      const taskToToggle = await fetchTask(id)
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+      const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type':'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json()
+
+      setTasks(tasks.map((task) => task.id === id
+      ? { ...task, reminder: data.reminder } : task))
+    }
+
+  return (
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTasks onAdd={addTask}/>}
+      {tasks.length > 0 ? (
+        <Tasks 
+        tasks={tasks} 
+        onDelete={delTasks}
+        onToggle={toggleReminder}/>
+       ) :  (
+         'No Tasks To Display'
+         )}
+    </div>
+  );
+}
+
+export default App;
